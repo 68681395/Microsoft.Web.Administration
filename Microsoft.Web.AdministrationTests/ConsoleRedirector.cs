25db52c3a329c3c5ba2fe88ced2d70d3132c3b33
68681395 <happycoder@foxmail.com>
2016-10-11 18:12:34 +0800

    daily upload

---
+++ Microsoft.Web.AdministrationTests/ConsoleRedirector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+// Copyright (c) Lex Li. All rights reserved.
+// 
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Web.AdministrationTests
+{
+    internal class ConsoleRedirector : IDisposable
+    {
+        private StringWriter _consoleOutput = new StringWriter();
+        private TextWriter _originalConsoleOutput;
+        public ConsoleRedirector()
+        {
+            this._originalConsoleOutput = Console.Out;
+            Console.SetOut(_consoleOutput);
+        }
+        public void Dispose()
+        {
+            Console.SetOut(_originalConsoleOutput);
+            Console.Write(this.ToString());
+            this._consoleOutput.Dispose();
+        }
+        public override string ToString()
+        {
+            return this._consoleOutput.ToString();
+        }
+    }
+}
